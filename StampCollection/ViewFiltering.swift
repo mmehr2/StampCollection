5b45e460cb085c90bf28956d3d86d47451c7ba26
Mike Mehr <mmehr2x-dev@yahoo.com>
2015-05-09 12:22:02 -0700

    Version using NSPredicate, which only solves part of the problem. YearRange, PriceRange, and the like require more dynamic processing of the fields before able to filter (limitations of predicate language, at least my understanding of it), so this version can't do those. A hybrid approach of the last two versions is suggested. So, the Predicates will limit by Category (incl.Multi), Keywords, and WantHave, which are easy to code in PL. And then Swift in-memory filtering and sorting of the NSManagedObject arrays will occur, perhaps using protocols.

--- StampCollection/ViewFiltering.swift
+++ StampCollection/ViewFiltering.swift
@@ -25,6 +25,14 @@ enum WantHaveType: Printable {
         }
     }
     
+    var code: String {
+        switch self {
+        case .All: return "?"
+        case .Haves: return "h"
+        case .Wants: return "w"
+        }
+    }
+    
     var description: String {
         switch self {
         case .All: return "All"
@@ -48,22 +56,25 @@ enum SearchType: Printable {
     case Location(String) // looks at any/all the location fields (type,ref,sec,page)
     case Price(String) // filtering based on price/value comparisons
     
-    var requiresBaseLookup : Bool {
-        switch self {
-        case Catalog: return true
-        case YearInRange: return true
-        case KeyWordListAll: return true
-        case KeyWordListAny: return true
-        case Price: return true
-        default: return false
-        }
-    }
-    
-    static func anyRequireBaseLookup( types: [SearchType] ) -> Bool {
-        return types.reduce(false) { (total, type) in
-            total || type.requiresBaseLookup
-        }
-    }
+//    var requiresBaseLookup : Bool {
+//        switch self {
+//        case Catalog: return true
+//        case YearInRange: return true
+//        case KeyWordListAll: return true
+//        case KeyWordListAny: return true
+//        case Price: return true
+//        default: return false
+//        }
+//    }
+//    
+//    static func anyRequireBaseLookup( types: [SearchType] ) -> Bool {
+//        let result = types.reduce(false) { (total, type) in
+//            total || type.requiresBaseLookup
+//        }
+//        let xxx = result ? "DO" : "DO NOT"
+//        println("Types \(types) \(xxx) require Base Lookup")
+//        return result
+//    }
     
     var description: String {
         switch self {
@@ -94,192 +105,143 @@ enum SearchType: Printable {
             return "Unknown search type"
         }
     }
-}
-
-// MARK: Individual search type functions - INFO
-// MARK: search Category
-protocol CatCompable {
-    var catgDisplayNum: Int16 {get}
-}
-
-extension DealerItem : CatCompable { }
-extension InventoryItem : CatCompable { }
-
-private func CompareCategory<T: CatCompable>( type: SearchType, item: T ) -> Bool {
-    var result = false
-    switch type {
-    case .Category(let catnum):
-        if catnum == CollectionStore.CategoryAll { result = true }
-        else { result = (item.catgDisplayNum == catnum) }
-    default:
-        break
-    }
-    return result
-}
-
-private func CompareMultiCategory<T: CatCompable>( type: SearchType, item: T ) -> Bool {
-    var result = false
-    switch type {
-    case .MultiCategory(let catnums):
-        // idea is to get OR of all categories in the list, so we run until we get a T (ret T) or F if all F
-        for catnum in catnums {
-            if CompareCategory( SearchType.Category(catnum), item ) {
-                // 1st true results in total truth
-                result = true
-                break
-            }
-        }
-    default:
-        break
-    }
-    return result
-}
-
-// MARK: search any string for keywords
-private func searchAny(input: String, For word: String) -> Bool
-{
-    if word == "" { return true }
-    if word[0] != "@" { return input.contains(word) }
-    // OPT: split word into two parts, optional leading TYPE field and rest is string to search for (word)
-    // this prefix would specify use of beginsWith, endsWith, or the CI versions of all three
-    // default is just plain "contains" searching
-    var wlen = count(word)
-    let codeSize = count("@@.@@") // general form of prefix code, . is a digit
-    if wlen > codeSize {
-        let code = word[0..<codeSize]
-        let wordPart = word[codeSize..<wlen]
-        switch code {
-        case "@@0@@": return input.containsCI(wordPart)
-        case "@@1@@": return input.beginsWith(wordPart)
-        case "@@2@@": return input.beginsWithCI(wordPart)
-        case "@@3@@": return input.endsWith(wordPart)
-        case "@@4@@": return input.endsWithCI(wordPart)
-        default: return input.contains(wordPart)
-        }
-    }
-    return input.contains(word)
-}
-
-private enum CompareStringType { case UseAny, UseAll }
-private func CompareStringForKeywords( input: String, words: [String], type: CompareStringType) -> Bool {
-    var result = false
-    switch type {
-    case .UseAny:
-        // idea is to get OR of all keywords, so we run until we get a T (ret T) or F if all F
-        for word in words {
-            if searchAny(input, For: word) {
-                // 1st true results in total truth
-                result = true
-                break
-            }
-        }
-    case .UseAll:
-        // idea is to get AND of all keywords, so we run until we get a F (ret F) or T if all T
-        result = true
-        for word in words {
-            if !searchAny(input, For: word) {
-                // 1st false results in total falsehood
-                result = false
-                break
-            }
-        }
-    default:
-        break
-    }
-    return result
-   
-}
-
-private func CompareDateInRange( input: String, range: ClosedInterval<Int> ) -> Bool {
-    let (fmt, extractedRange) = extractYearRangeFromDescription(input)
-    if fmt != 0 {
-        let startInside = range.contains(extractedRange.start)
-        let endInside = range.contains(extractedRange.end)
-        if startInside && endInside {
-            // fully contained
-            return true
-        }
-    }
-    return false
-}
-
-// MARK: search for INFO
-private func CompareInfoSingle( types: [SearchType], item: DealerItem) -> Bool {
-    var result = true
-    for type in types {
-        var eachResult = true
-        switch type {
-        case .Category(_): eachResult = CompareCategory( type, item )
-        case .MultiCategory(_): eachResult = CompareMultiCategory( type, item )
-        case .KeyWordListAll(let wordList):
-            eachResult = CompareStringForKeywords(item.descriptionX, wordList, .UseAll)
-        case .KeyWordListAny(let wordList):
-            eachResult = CompareStringForKeywords(item.descriptionX, wordList, .UseAny)
+    
+    func getPredicateForDataType( dataType: CollectionStore.DataType ) -> NSPredicate? {
+        var output: NSPredicate?
+        let dealerItemPrefix = dataType == .Inventory ? "dealerItem." : ""
+        switch self {
+        case .None:
+            //output = NSPredicate(format: "TRUEPREDICATE")
+            break
+        case .Category(let catnum):
+            // same for both INFO and INV
+            output = NSPredicate(format: "%K = %@", "catgDisplayNum", NSNumber(short: catnum))
+            break
+        case .KeyWordListAll(let words):
+            // naive implementation: AND a bunch of predicates, one for each key-to-word-value pair
+            // actually this will NOT work, since it requires ALL the words to appear entirely in one of 3 fields
+            // it will NOT allow mix-and-matching - ONLY WAY TO DO THIS IS CONCATENATE THE FIELDS FOR SEARCHING!
+            // Until I can figure out how to do this, it will only check descriptionX in the dealerItem
+            let target1 : String = dealerItemPrefix + "descriptionX"
+            var subpredicates1 = getPredicatesForField(target1, inKeyWordList: words)
+            var temp = NSCompoundPredicate.andPredicateWithSubpredicates(subpredicates1)
+//            var subpredicates2 = dataType != .Inventory ? [] : (getPredicatesForField("desc", inKeyWordList: words))
+//            if subpredicates2.count > 0 {
+//                let out2 = NSCompoundPredicate.andPredicateWithSubpredicates(subpredicates2)
+//                temp = NSCompoundPredicate.orPredicateWithSubpredicates([out2, temp])
+//            }
+//            var subpredicates3 = dataType != .Inventory ? [] : (getPredicatesForField("notes", inKeyWordList: words))
+//            if subpredicates3.count > 0 {
+//                let out3 = NSCompoundPredicate.andPredicateWithSubpredicates(subpredicates3)
+//                temp = NSCompoundPredicate.orPredicateWithSubpredicates([out3, temp])
+//            }
+            output = temp
+            break
+        case .KeyWordListAny(let words):
+            // naive implementation: OR a bunch of predicates, one for each key-to-word-value pair
+            // for INV, also check the "desc" and "notes" fields
+            // if ANY word appears in ANY field, the entire predicate is true
+            let target1 : String = dealerItemPrefix + "descriptionX"
+            let cival = true // not really sure how to use this, so just try it for now on ANY searches
+            var subpredicates1 = getPredicatesForField(target1, inKeyWordList: words, caseInsensitive: cival)
+            var subpredicates2 = dataType != .Inventory ? [] : (getPredicatesForField("desc", inKeyWordList: words, caseInsensitive: cival))
+            var subpredicates3 = dataType != .Inventory ? [] : (getPredicatesForField("notes", inKeyWordList: words, caseInsensitive: cival))
+            var subpredicates = subpredicates1 + subpredicates2 + subpredicates3
+            var temp = NSCompoundPredicate.orPredicateWithSubpredicates(subpredicates)
+            output = temp
+            break
+        /** NOTE: The reason this and other fetches like this aren't here is the need to use derived data and not just what is in the fields using the predicate programming language
+            I cannot figure out a way to do this in a way that preprocesses the field, such as creating transient properties (cannot be used in predicates or sort descriptors)
+            Thus, I am forced to implement features like this in Swift as a second step filtering; luckily the 1st step is quick and reduces the size of the data (WantHave, Category and KeyWordList)
+            For more complicated searching and sorting, we will rely on Swift on the smaller data arrays. This assumption breaks down for the All Categories choice, but the user will wait when managing the entire dataset.
+            
         case .YearInRange(let range):
-            eachResult = CompareDateInRange(item.descriptionX, range)
-        default: eachResult = true
-        }
-        if !eachResult {
-            // quit on 1st negative (ALL responses must be true to include item)
-            result = false
+            // prefix allows this to work with both INFO and INV
+            let key1 = dealerItemPrefix + "exYearStart"
+            let key2 = dealerItemPrefix + "exYearEnd"
+            let val1 = NSNumber(integer: range.start)
+            let val2 = NSNumber(integer: range.end)
+            output = NSPredicate(format: "%K != 0 AND %K != 0 AND %K >= %@ AND %K <= %@", key1, key2, key1, val1, key2, val2)
+            break
+            **/
+        case .WantHave(let whtype):
+            // works for INV only
+            let key = "wantHave"
+            let value = whtype.code
+            if value != "?" {
+                output = NSPredicate(format: "%K = %@", key, value)
+            }
+            break
+        case .MultiCategory(let catnums):
+            // works with either INFO or INV
+            var preds : [NSPredicate] = []
+            for catnum in catnums {
+                if let pred = SearchType.Category(catnum).getPredicateForDataType(dataType) {
+                    preds.append(pred)
+                }
+            }
+            if preds.count > 0  {
+                output = NSCompoundPredicate.orPredicateWithSubpredicates(preds)
+            }
+            break
+//        case .SubCategory:
+//            output = NSPredicate(format: "TRUEPREDICATE")
+//            break
+//        case .Catalog:
+//            output = NSPredicate(format: "TRUEPREDICATE")
+//            break
+//        case .Location:
+//            output = NSPredicate(format: "TRUEPREDICATE")
+//            break
+//        case .Price:
+//            output = NSPredicate(format: "TRUEPREDICATE")
+//            break
+        default:
+            //output = NSPredicate(format: "TRUEPREDICATE")
             break
         }
-    }
-    return result
-}
-
-func filterInfo( coll: [DealerItem], types: [SearchType] ) -> [DealerItem] {
-    return types.count == 0 ? coll : coll.filter { item in
-        return CompareInfoSingle( types, item )
+        return output
     }
 }
 
-// MARK: Individual search type functions - INVENTORY
-private func CompareInvWantHave( type: SearchType, item: InventoryItem ) -> Bool {
-    var result = false
-    switch type {
-    case .WantHave(let wht):
-        if wht == .Haves && item.wantHave == "h" { result = true }
-        if wht == .Wants && item.wantHave == "w" { result = true }
-        break
-    default:
-        break
+private func getPredicatesForField( field: String, inKeyWordList words: [String], caseInsensitive cival: Bool = false ) -> [NSPredicate] {
+    var output : [NSPredicate] = []
+    let command = cival ? "CONTAINS[c]" : "CONTAINS"
+    for word in words {
+        output.append( NSPredicate(format: "%K %s %@", field, command, word) )
     }
-    return result
+    return output
 }
 
-// MARK: search for INVENTORY
-private func CompareInvSingle( types: [SearchType], item: InventoryItem) -> Bool {
-    var result = true
-    var baseItem: DealerItem?
-    if SearchType.anyRequireBaseLookup(types) {
-        baseItem = CollectionStore.sharedInstance.fetchInfoItem(item.baseItem)
-    }
+func getPredicateOfType( dataType: CollectionStore.DataType, forSearchTypes types: [SearchType] ) -> NSPredicate? {
+    var output : NSPredicate?
+    // get the combined AND of all non-nil predicates (simplest use case)
     for type in types {
-        var eachResult = true
-        switch type {
-        case .Category(_): eachResult = CompareCategory( type, item )
-        case .MultiCategory(_): eachResult = CompareMultiCategory( type, item )
-        case .KeyWordListAll(let wordList):
-            eachResult = CompareStringForKeywords(baseItem!.descriptionX + item.desc + item.notes, wordList, .UseAll)
-        case .KeyWordListAny(let wordList):
-            eachResult = CompareStringForKeywords(baseItem!.descriptionX + item.desc + item.notes, wordList, .UseAny)
-        case .YearInRange(let range):
-            eachResult = CompareDateInRange(baseItem!.descriptionX, range) // date extraction only works on baseItem description field
-        case .WantHave(_): eachResult = CompareInvWantHave( type, item )
-        default: eachResult = true
-        }
-        if !eachResult {
-            // quit on 1st negative (ALL responses must be true to include item)
-            result = false
-            break
+        if let pred = type.getPredicateForDataType(dataType) {
+            if output == nil {
+                output = pred
+            } else {
+                output = output! && pred
+            }
         }
     }
-    return result
+    return output
 }
 
-func filterInventory( coll: [InventoryItem], types: [SearchType] ) -> [InventoryItem] {
-    return types.count == 0 ? coll : coll.filter { item in
-        return CompareInvSingle( types, item )
-    }
+// simple predicate extensions to deal with making compound predicates
+infix operator && {}
+func &&( lhs: NSPredicate, rhs: NSPredicate) -> NSPredicate {
+    return NSCompoundPredicate.andPredicateWithSubpredicates([lhs, rhs])
 }
+
+infix operator || {}
+func ||( lhs: NSPredicate, rhs: NSPredicate) -> NSPredicate {
+    return NSCompoundPredicate.orPredicateWithSubpredicates([lhs, rhs])
+}
+
+prefix operator ! {}
+prefix func !( lhs: NSPredicate) -> NSPredicate {
+    return NSCompoundPredicate.notPredicateWithSubpredicate(lhs)
+}
+
+
