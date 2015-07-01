b284fd82cfc64f4cdbe925439ae11b9f8f7133d7
Mike Mehr <mmehr2x-dev@yahoo.com>
2015-07-01 16:19:44 -0700

    Attempt to add working device support for sending the three CSV files in a custom SSZipArchive with extension ".SCZP" (Stamp Collection ZiP), which should allow registration of the custom file extension for Import usage. Unfortunately, doesn't work. File includes detailed crash notes.

--- StampCollection/EmailAttachmentExporter.swift
+++ StampCollection/EmailAttachmentExporter.swift
@@ -12,6 +12,24 @@ import MessageUI
 // this object will invoke the Apple UI to send a preset email message with attachments the user can edit before sending
 // NOTE: Apple does NOT allow full programmatic email sends
 // NOTE: Causes Simulator to crash, due to no Email application installed. Need a device!
+/*
+IDEA: To send images, use Dropbox to send a big ZIP file. Here's a framework to use Dropbox2 API from Swift:
+https://github.com/dropbox/swiftydropbox
+
+CRASH NOTES:
+I tried this on my iPhone 6: various misbehaviors happened.
+1) The VC comes up. If I Send the email, it goes out but the VC doesn't dismiss and the delegate callback doesn't get called.
+2) If I Cancel, when I Delete Draft, the app crashes without calling the callback. (GPFLT at AppDelegate line 1) EXC_BAD_ACCESS code=1 adr=<random 0xdca5beb8 (note that "beb8" again!)
+3) If I Cancel, and Save Draft, same crash and no callback. EXC_BAD_ACCESS code=1 adr=0x188beb8
+4) On Simulator, if I Send, the app crashes same way (GPFLT on AppDelegate line 1). code=1 adr=0x18
+5) On Simulator, Cancel/Delete Draft, app runs callback properly but crashes trying to do errFunc()
+  But then it crahsed again, but code=EXC_I386_GPFLT no adr given
+6) On Simulator, Cancel/Save Draft, same crash (GPFLT on AppDelegate line 1). code=1 adr=0x18
+
+From about 10 test runs on device and sim, with various paths, I conclude:
+On device, crash is intermittent and only on Cancel (either way) - Send always sends but will not dismiss the VC or get to my breakpoint.
+On simulator, crash has also happened intermittently, but when it passes breakpoint, it fails in the errFunc() call.
+*/
 
 class EmailAttachmentExporter: NSObject, MFMailComposeViewControllerDelegate {
     var controller: UIViewController!
@@ -25,19 +43,21 @@ class EmailAttachmentExporter: NSObject, MFMailComposeViewControllerDelegate {
     
     func sendFiles(fileSuffix: String = "") -> Bool {
         // The files to send are: CATEGORY.CSV, INFO.CSV, INVENTORY.CSV from the Documents directory
-        // The file suffix is an optional string to add to each base name (INFOxxx.CSV)
+        // The file suffix is an optional string to add to each base name (INFOxxx.CSV) in case we use multiple sets of files for user backup
+        var ecode = 0
         
         let now = NSDate()
         let datestr = getFormattedStringFromDate(now, withTime: true)
-        let emailSubject = "Stamp Collection Backup for \(datestr)"
+        let versionstr = "v1.0"
+        let emailSubject = "Stamp Collection Backup \(versionstr) at \(datestr)"
 
-        let fileExtension = ".CSV"
-        let file1 = "CATEGORY" + fileSuffix + fileExtension
-        let file2 = "INFO" + fileSuffix + fileExtension
-        let file3 = "INVENTORY" + fileSuffix + fileExtension
+        let fileExtension = ".csv"
+        let file1 = "category" + fileSuffix + fileExtension
+        let file2 = "info" + fileSuffix + fileExtension
+        let file3 = "inventory" + fileSuffix + fileExtension
         let emailBody = "Sending the following attachments:\n\t\(file1)\n\t\(file2)\n\t\(file3)\n"
         
-        let recipient1 = "mmehr2@yahoo.com" // make this a setting
+        let recipient1 = "mmehr2@yahoo.com" // TBD: make this a setting
         let toRecipients = [recipient1]
         
         let ad = UIApplication.sharedApplication().delegate! as! AppDelegate
@@ -52,20 +72,48 @@ class EmailAttachmentExporter: NSObject, MFMailComposeViewControllerDelegate {
         let file2exists = fileManager.fileExistsAtPath(file2url.path!)
         let file3exists = fileManager.fileExistsAtPath(file3url.path!)
         if !(file1exists && file2exists && file3exists) {
+            if !file1exists { ecode += 1; println("Error preparing for email export: \(file1url.path!) doesn't exist.") }
+            if !file2exists { ecode += 2; println("Error preparing for email export: \(file2url.path!) doesn't exist.") }
+            if !file3exists { ecode += 4; println("Error preparing for email export: \(file3url.path!) doesn't exist.") }
             if let errFunc = errFunc {
-                errFunc( NSError(domain: "StampCollection", code: 1, userInfo: nil) )
+                errFunc( NSError(domain: "StampCollection", code: ecode, userInfo: nil) )
             }
-            if !file1exists { println("Error preparing for email export: \(file1) doesn't exist.") }
-            if !file2exists { println("Error preparing for email export: \(file2) doesn't exist.") }
-            if !file3exists { println("Error preparing for email export: \(file3) doesn't exist.") }
             return false
         }
 
-        let file1Data = NSData(contentsOfURL: file1url)
-        let file2Data = NSData(contentsOfURL: file2url)
-        let file3Data = NSData(contentsOfURL: file3url)
+        // replace with SSZipArchive
+        let zipFileExtension = ".sczp" // can be used for custom registration for import feature
+        let zipFilename = "StampCollection" + zipFileExtension
+        let zipFileurl = ad.applicationDocumentsDirectory.URLByAppendingPathComponent(zipFilename)
+        var error : NSError?
+        fileManager.removeItemAtURL(zipFileurl, error: &error)
+        if error != nil {
+            ecode += 8
+            println("Unable to remove existing ZIPFILE: \(error!.localizedDescription).")
+        }
+        let fileUrls = [file1url.path!, file2url.path!, file3url.path!]
+        let zipArchiveOK = SSZipArchive.createZipFileAtPath(zipFileurl.path!, withFilesAtPaths:fileUrls)
+        if !zipArchiveOK {
+            ecode += 16
+            println("Unable to create ZIPFILE archive \(zipFilename): \(error!.localizedDescription).")
+            if let errFunc = errFunc {
+                errFunc( NSError(domain: "StampCollection", code: ecode, userInfo: nil) )
+            }
+            return false
+        }
         
-        let mimeType = "text/csv"
+        let fileData = NSData(contentsOfURL: zipFileurl)
+        
+        let mimeType = "application/zip"
+        
+        // make sure email feature is available (running on device)
+        if !MFMailComposeViewController.canSendMail() {
+            ecode += 32
+            if let errFunc = errFunc {
+                errFunc( NSError(domain: "StampCollection", code: ecode, userInfo: nil) )
+            }
+            return false
+        }
         
         var mc = MFMailComposeViewController()
         mc.mailComposeDelegate = self
@@ -73,9 +121,7 @@ class EmailAttachmentExporter: NSObject, MFMailComposeViewControllerDelegate {
         mc.setMessageBody(emailBody, isHTML: false)
         mc.setToRecipients(toRecipients)
         
-        mc.addAttachmentData(file1Data, mimeType: mimeType, fileName: file1)
-        mc.addAttachmentData(file2Data, mimeType: mimeType, fileName: file2)
-        mc.addAttachmentData(file3Data, mimeType: mimeType, fileName: file3)
+        mc.addAttachmentData(fileData, mimeType: mimeType, fileName: zipFilename)
         
         // present the VC on behalf of the provided VC
         controller.presentViewController(mc, animated: true, completion: nil)
@@ -85,6 +131,7 @@ class EmailAttachmentExporter: NSObject, MFMailComposeViewControllerDelegate {
     func mailComposeController(controller: MFMailComposeViewController!, didFinishWithResult result: MFMailComposeResult, error: NSError!)
     {
         var errHappened = false
+        let fakeError = NSError(domain: "StampCollection", code: 0, userInfo: nil)
         switch result {
         case MFMailComposeResultCancelled:
             println("Email canceled by user")
@@ -105,14 +152,20 @@ class EmailAttachmentExporter: NSObject, MFMailComposeViewControllerDelegate {
         
         // dismiss the VC
         controller.dismissViewControllerAnimated(true) {
-            if let errFunc = self.errFunc where errHappened {
+            // try doing nothing here
+        }
+        if let errFunc = self.errFunc {
+            if errHappened {
                 errFunc(error)
+            } else {
+                errFunc(fakeError)
             }
         }
     }
 }
 
-// why doesn't Apple define this?? and why the constants and not an ENUM???
+// NOTE: function required to allow result to be used in a switch statement (check if still needed: Swift 1.2)
+// why doesn't Apple define this?
 func ~=( lhs: MFMailComposeResult, rhs: MFMailComposeResult) -> Bool {
     return lhs.value ~= rhs.value
 }
