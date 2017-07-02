1561a1315735b536cbda1575ffc356ba965cdb32
Mike Mehr <mmehr2x-dev@yahoo.com>
2017-07-02 02:14:06 -0700

    Implemented full validation feature for Inventory parts of UIQueryAlert. Will rerun AC in case of errors as defined by validation routine, with old contents of fields and a status label describing what is in error. Added Cancel button. Used tags for fields instead of comparing identities. Instead of field-by-field validation, I chose to validate when OK is pressed (or keyboard Done when in final field). This allows easier customization by type as well as partial entries. Might want to revisit this later.

--- StampCollection/UIQueryAlert.swift
+++ StampCollection/UIQueryAlert.swift
@@ -156,6 +156,7 @@ class UIQueryAlert: NSObject, UITextFieldDelegate {
     
     var config: UIQueryAlertConfiguration
     var fields: [UITextField] = []
+    var status: UITextField!
     var userCompletionHandler: ((SearchType) -> Void)?
 
     init(type: UIQueryAlertType, completion: @escaping (SearchType) -> Void) {
@@ -165,9 +166,13 @@ class UIQueryAlert: NSObject, UITextFieldDelegate {
         super.init()
     }
 
-    func RunWithViewController(_ vc: UIViewController) {
+    func RunWithViewController(_ vc: UIViewController, withStatus statstr: String = "", andPresets presets: [String] = []) {
         fields = []
         let ac = UIAlertController(title: config.title, message: config.body, preferredStyle: .alert)
+        let can = UIAlertAction(title: "Cancel", style: .cancel) { x in
+            // do nothing here
+        }
+        ac.addAction(can)
         let act = UIAlertAction(title: "OK", style: .default) { x in
             // run the user's completion handler in response to OK being pressed
             if let handler = self.userCompletionHandler {
@@ -204,20 +209,29 @@ class UIQueryAlert: NSObject, UITextFieldDelegate {
                     }
                     result = SearchType.yearInRange(startYear...endYear)
                     handler(result)
-                case .invAskSection:
-                    // KLUDGE - we need to use a SearchType to return the textual result
+                case .invAskSection(let namelist), .invAskAlbum(let namelist):
+                    // KLUDGE - we need to use a SearchType.location to return the textual result
                     let text : String = self.fields[0].text ?? ""
+                    let statcode = self.validateInvUsage(forText: text, in: namelist)
+                    if statcode != .OK {
+                        let statstr = "Name is: \(statcode)"
+                        self.RunWithViewController(vc, withStatus: statstr, andPresets: [text])
+                        return
+                    }
                     result = SearchType.location(text)
                     handler(result)
-                case .invAskAlbum:
-                    // KLUDGE - we need to use a SearchType to return the textual result
-                    let text : String = self.fields[0].text ?? ""
-                    result = SearchType.location(text)
-                    handler(result)
-                case .invAskSectionAndAlbum:
-                    // KLUDGE - we need to use a SearchType to return the textual result
+                case .invAskSectionAndAlbum(let namelist1, let namelist2):
+                    // KLUDGE - we need to use a SearchType.location to return the textual result
                     let text1 : String = self.fields[0].text ?? ""
                     let text2 : String = self.fields[1].text ?? ""
+                    let statcode1 = self.validateInvUsage(forText: text1, in: namelist1)
+                    let statcode2 = self.validateInvUsage(forText: text2, in: namelist2)
+                    if statcode1 != .OK || statcode2 != .OK {
+                        let statstr = "Section is: \(statcode1); Album is: \(statcode2)"
+                        self.RunWithViewController(vc, withStatus: statstr, andPresets: [text1, text2])
+                        return
+                    }
+                    // the double result is returned as a string delimited by a semicolon (disallow semicolons in names)
                     let text = "\(text1);\(text2)"
                     result = SearchType.location(text)
                     handler(result)
@@ -225,24 +239,39 @@ class UIQueryAlert: NSObject, UITextFieldDelegate {
             }
         }
         ac.addAction(act)
+        // add a status label
+        ac.addTextField() { (statusField) in
+            statusField.delegate = self
+            statusField.tag = -1
+            statusField.text = statstr
+            self.status = statusField
+        }
         // add the text fields required by the configuration
         for index in 0..<config.getConfigurationCount() {
             let fldconfig = config.getConfiguration(index)
             ac.addTextField() { (textField) in
                 textField.delegate = self
                 textField.placeholder = fldconfig.placeholder
+                if presets.count > index && !presets[index].isEmpty {
+                    textField.text = presets[index]
+                }
+                textField.tag = index
                 if fldconfig.type == .numeric {
                     textField.keyboardType = .numbersAndPunctuation //.NumberPad
                 } else if fldconfig.type == .text {
                     textField.keyboardType = .default //.ASCIICapable
                     switch self.config.type {
-                    case UIQueryAlertType.invAskSection, UIQueryAlertType.invAskAlbum, UIQueryAlertType.invAskSectionAndAlbum:
+                    case .invAskSection, .invAskAlbum, .invAskSectionAndAlbum:
                         textField.autocapitalizationType = .allCharacters
                     default:
                         break
                     }
                 }
                 textField.returnKeyType = .done
+                if index == 0 {
+                    // NOTE: this is automatically called the first time, but not for validation retries, so best to do it here
+                    textField.becomeFirstResponder()
+                }
                 self.fields.append(textField)
             }
         }
@@ -259,20 +288,66 @@ class UIQueryAlert: NSObject, UITextFieldDelegate {
     
     // MARK: - UITextFieldDelegate
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
+        // runs when keyboard Done is pressed
         return true
     }
     
     func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        if textField.tag < 0 {
+            return false // disallow editing the status label
+        }
         return true
     }
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
-        textField.becomeFirstResponder()
+        //textField.becomeFirstResponder() // not needed here
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
         //updateModel() // name field updated and keyboard dismissed
+        print("In textFieldDidEndEditing for field #\(textField.tag + 1)")
+        //textField.resignFirstResponder()
+    }
+    
+    enum InvValidationType: CustomStringConvertible {
+        case OK, empty, inUse, badUse
+        var description: String {
+            switch self {
+            case .OK:
+                return "OK"
+            case .empty:
+                return "Empty"
+            case .inUse:
+                return "In Use"
+            default:
+                return "Bad Use"
+            }
+        }
+    }
+    
+    private func validateInvUsage(forText text: String?, in names: String) -> InvValidationType {
+        if let text=text {
+            if text.isEmpty {
+                print("Name is empty, nonblank required. Please try again.")
+                return .empty
+            }
+            let pnames = names.components(separatedBy: ",")
+            if pnames.contains(text) {
+                print("Name \(text) is in list \(pnames); try again.")
+                return .inUse
+            }
+            print("Name \(text) is validated (not in) \(pnames)")
+            return .OK
+        } else {
+            print("No text entered. Please try again.")
+            return .badUse
+        }
+   }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        // validation traditionally should occur here - return true if AOK, false if something needs correction
+        // NOTE: I have chosen to implement full validation with the OK button press
+        return true
     }
 
 }
