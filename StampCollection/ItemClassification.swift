f825c715ee1c4b8c109bcac26cc257b9ab56b02c
Mike Mehr <mmehr2x-dev@yahoo.com>
2019-09-25 22:32:58 -0700

    INFO: fixed SHEETS11 errata; fixed mis-spellings of "se-tenant", "Sports", and "Breastplate" in many items (sets, info folders, sheets). INV: entered SHEETS11 album data along with needed fixes to it.

--- StampCollection/ItemClassification.swift
+++ StampCollection/ItemClassification.swift
@@ -22,4 +22,19 @@ enum ItemClassification {
     case ImperfSouvenirSheet // sasme as S/S but is imperforated (and usually numbered)
     case ImperfSheet // single sheet, imperforated
     case ImperfSheetSet(Int) // set of imperf sheets related by a common design, usually issued as a unit
+    case ImperfDesignerSheet // BAD NAME: basically the gigantic boxed-set sheet of multiple sheets that imperf sets are cut from
+}
+
+
+func extractSheetDetailsFromSheetDescription( _ descr: String ) -> (String) {
+    var result = ""
+    // get the details string out of a Full Sheet auto-generated description (cat.31)
+    // 1. (multi) "1989 Tourism- Full sheet (#1/4) [Pl.No.71 (1986+), Format=(3x5)] Design:O.E. Schwarz"
+    // 2. (single) "2004 TELABUL 2004 - 'Design a Stamp'- Full sheet [Pl.No.570 (1986+), Format=(3x5)] Design:Michel Kishka"
+    let pnr = descr.components(separatedBy: "- Full sheet ")
+    if (pnr.count > 1) {
+        // pnr[0] is the main title, while pnr[1] is what we want
+        result = pnr[1]
+    }
+    return result
 }
